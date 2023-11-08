33b0cff4a2f23eb09b9b09a20d19b48d657103c8
Danielle Williams <daniellewilliams@Danielles-MBP.lan>
2023-11-08 11:32:21 -0500

    new doumentation and adjustments to enterloop

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 //I tried a few different APIs, but I just need like normie five letter words, so I made my own file, it's got...lots of 5-letter words in it!
 import { wordArray } from "./words.js";
-
 document.addEventListener('DOMContentLoaded', ()=>{
 //randomly selects a word from our...many words.
 let generateWord = ()=>{
@@ -13,6 +12,7 @@ let generateWord = ()=>{
 }
 //the word our game will be played with
 let newWord = generateWord()
+console.log(newWord)
 //let newWord = "inbox"
 
 
@@ -61,18 +61,19 @@ const alphabetArray = keyboardOneArray.concat(keyboardTwoArray, keyboardThreeArr
 
 //function to submit new word 
 async function checkWord(){
-    //turn current attempt into just a number for easy evaluation and incrimenting
+    //turn current attempt into a number for easy evaluation and incrimenting
+    //this is a sting with a hiphenated number, so the last index should always be a number
     let attempt = Number(currentAttempt[currentAttempt.length-1])
-    //this will be assigned with a function 5= win anything less = not win
+    //this will be assigned with a function 5 = win anything less = not win
     let winCount
-    //First we check to be sure the work is long enough...
+    //First we check to be sure the word is long enough...
     if(enteredWord.length === 5){
         //then we run an API call to the dictionary to make sure it's actually a word...
         try{ 
             const res = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${enteredWord.join("")}`)
             const result = await res.json()
             if(result[0]){ 
-                //if it is a word we send it to our win loop to see if it's the winning word
+                //if it is a word we run our win loop to see if it's the winning word
                 winCount = enterLoop()
                 //if it is the winning word, we give a win message dependent of the players current attempt
                 if(winCount === 5){
@@ -97,7 +98,7 @@ async function checkWord(){
                     }
 
                 }
-            //if the word is not long enough, we tell the player such
+            //if the word is not a word, we tell the player
             }else{
                 dialogeBox.textContent = "That's not a word!"
                 gsap.to(dialogeBox, {display:"block", repeat:1, yoyo:true})
@@ -146,12 +147,12 @@ function handleArray(array, keyBoardRow){
 function enterLoop(){
     //gsap timeline to ensure animation happens one letter at a time.
     const tl = gsap.timeline()
-    //will keep track of the correctlky guessed and placed letters, if it gets to five they win
+    //will keep track of the correctly guessed and placed letters, if it gets to five they win
     let winner = 0
-    //get the row we are one
+    //get the row we are on
     let row = document.getElementById(currentAttempt)
     //get the spaces to check
-    let spacesToCheck = row.children
+    let spacesToCheck = [...row.children]
     //split the new word into an array, this is the word the user is trying to guess
     let newWordArray = newWord.split("")
     const foundLetters  = []
@@ -161,8 +162,13 @@ function enterLoop(){
        let letterToCheck = spacesToCheck[i].textContent
         //tracks the color change of each inner loop
         let color
-        //checks the amount of thimes a letter appears in a word
-        // these variable will depend on the row where the letter is on the keyboard since there are three different keyboard arrays and are responsible for finding the keys on the keyboard that need to change colors
+        //checks the amount of times each letter appears in the new word
+        let countArray = newWordArray.map(letter=>{
+            var count = 0;
+            newWordArray.forEach((v) => (v === letter && count++));
+            return count;
+        })
+        // these variable will depend on the row where the letter is on the keyboard since there are three different keyboard arrays that are responsible for finding the keys on the keyboard that need to change colors
         let keyboardPlace
         let keyboardRow
         //first row
@@ -181,22 +187,23 @@ function enterLoop(){
         //we will loop through each letter of the new word. 
         //This loop will set the color of the letter being checked. a green color returned means the leter was found in the right place, yellow means it was found in the wrong place, grey means it was not found
        for(let x = 0; x <newWordArray.length; x++){
+        //get's the number of appearances of each letter in the new word from peviousl defined array
+        const appearences = countArray[x]
         //get each letter from the new word array
             let letterToCheckAgainst = newWordArray[x]
-            //if the letter that we are on in spaces to check loop is eqaul to the letter we are on in the new word loop
+            //if the letter that we are on in spaces to check loop is equal to the letter we are on in the new word loop
             if(letterToCheck === letterToCheckAgainst.toUpperCase()){
                 //..futhermore if they are in the same positions...
                 if(i===x){
                     //change the color green
                     foundLetters.push(letterToCheckAgainst.toUpperCase())
                     color = "#69b969"
+                    winner++
                     {break}
                     //if they are not in the same positions...
                 }else{
-                    //it first checks to see how many times the letterToCheck(from our entered word) is in the new Word
-                    let letterCheckArray = newWordArray.filter((letter)=>letter.toUpperCase()===letterToCheck.toUpperCase())
-                    //if we already found the letter we are checking AND it appears in the new word once of less 
-                    if(foundLetters.indexOf(letterToCheck) >= 0 && letterCheckArray.length <=1){
+                    //if we already found the letter we are checking AND it appears in the new word once or less 
+                    if(foundLetters.indexOf(letterToCheck) >= 0 && appearences <=1){
                         //change the color grey
                         color = "grey" 
                         //in all other cases...
@@ -218,11 +225,6 @@ function enterLoop(){
                     color = "grey" 
                 }
             }
-       }
-       //if the color returned is green...
-       if(color === "#69b969"){
-        // add 1 to winner, if this is five at the end of the loop, the player has found the word. 
-            winner++
        }
        //use gsap to change the color of the keys on the gameboard and keyboard
        tl.to(spacesToCheck[i], {background:color, duration:.75})
